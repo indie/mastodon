dbc7638c82138437baf2df36550f7ab652f0a540
Leona C <lsc@hackeress.com>
2019-07-06 01:56:10 -1000

    Fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # English when a translation cannot be found).
-  config.i18n.fallback = [:en]
+  # config.i18n.fallback = [:en]
   config.i18n.fallbacks = [:en]
 
   # Send deprecation notices to registered listeners.
